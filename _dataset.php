a8b24cb3916ac9c3bd14fa8a3108d25773c0e412
Nathalie Castells-Brooke <nathalie.castells@rothamsted.ac.uk>
2024-03-21 13:12:01 +0000

    LAT and update metadata

--- _dataset.php
+++ _dataset.php
@@ -24,13 +24,11 @@ $dsinfo = $GLOBALS['dsinfo']; //added 2023-02-21 to circumvent undefined variabl
 	<div id="greenTitle" class="d-flex  py-3 p3-3 bg-primary text-white mt-0 ">
 		<h1 class="mx-3"><?php echo $datasetTitle?> </h1>
 
-
 	</div>
 	<div class="row m-3">
 		<div class=" p-3 border border-success rounded b">
 			<div class="pb-3 "><strong>Citation: &nbsp;</strong><?php echo $refAuthor; ?> (<?php echo $year;?>).
 				<?php echo $dsinfo['name'];?> <em><?php echo $getPublisher; ?></em>
-
 				<a target="_blank" href="<?php echo $actualURL;?>"><?php echo $dsinfo['identifier'];?></a></div>
 
 			<!-- The text field -->
@@ -54,10 +52,6 @@ $reference = $refAuthor. " (".$year.") ". $dsinfo['name']. " - ". $getPublisher 
 		echo $enwURL;
 	}?>
 			</div>
-
-
-
-
 		</div>
 	</div>
 	<div class="row mx-0 mb-3">
@@ -68,11 +62,8 @@ $reference = $refAuthor. " (".$year.") ". $dsinfo['name']. " - ". $getPublisher 
 			} else {
 		?>
 		<div class="col-sm-4">
-
-
 			<div class="card card-summary ">
 				<div class="card-body">
-
 					<ul class="list-group list-group-flush ">
 						<?php
 
@@ -90,9 +81,6 @@ $reference = $refAuthor. " (".$year.") ". $dsinfo['name']. " - ". $getPublisher 
 
     	if ($dsinfo['isExternal'] == 0) { echo $strUserArea;
         ?>
-
-
-
 						<li class="list-group-item"><b>DOI: </b><a
 								href="https://doi.org/<?php echo $dsinfo['identifier'];?>"><?php echo $dsinfo['identifier'];?></a>
 						</li>
@@ -146,8 +134,6 @@ $reference = $refAuthor. " (".$year.") ". $dsinfo['name']. " - ". $getPublisher 
 			</div>
 		</div>
 		<div class="col-sm-8">
-
-
 
 			<p class="text-right">
 				<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button"
@@ -212,9 +198,12 @@ if ($dsinfo['isExternal'] == 0) {
 
 			<h4>License</h4>
 			<p>
-				<a rel="license" target="_blank" href="http://creativecommons.org/licenses/by/4.0/" target="out"><img
-						style="width: 50px;" alt="Creative Commons License" src="images/logos/cc4.png"
-						align="middle" /></a> This dataset is
+				<a rel="license" target="_blank" href="http://creativecommons.org/licenses/by/4.0/" target="out">
+					<img
+						style="width: 50px;" 
+						alt="Creative Commons License" 
+						src="images/logos/cc4.png"
+						/></a> This dataset is
 				available under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons
 					Attribution Licence (4.0)</a>.
 			</p>
@@ -224,13 +213,11 @@ if ($dsinfo['isExternal'] == 0) {
 				<?php echo $datasetTitle;?> <em><?php echo $getPublisher; ?></em>
 				<a target="_blank"
 					href="https://doi.org/<?php echo $dsinfo['identifier'];?>">https://doi.org/<?php echo $dsinfo['identifier'];?></a>
-
-
-				<p>
+			<p>
 					Please review our <a href="info/howtocredit">How to Credit Datasets</a>
 					guidance for more information.
 				</p>
-				<h5>Conditions of Use</h5>
+				<h4>Conditions of Use</h4>
 
 				<p>
 					Rothamsted relies on the integrity of users to ensure that datasets
@@ -341,8 +328,8 @@ if ($dsinfo['isExternal'] == 0) {
 											Long-Term Experiments, </A> the <a
 											href=" https://www.rothamsted.ac.uk/facilities-and-resources/rothamsted-sample-archive">
 											Rothamsted Sample Archive </a> and Rothamsted's environmental monitoring
-										activities including the weather stations and its role in the <ahref="https:
-											//www.rothamsted.ac.uk/facilities-and-resources/environmental-change-network">
+										activities including the weather stations and its role in the <a
+											href="https://www.rothamsted.ac.uk/facilities-and-resources/environmental-change-network">
 											UK Environmental Change Network</A>
 
 									</li>
@@ -495,7 +482,8 @@ if ($dsinfo['isExternal'] == 0) {
 					<div class="form-group">
 						<label class="form-check-label" for="understandCheck"> <b>e-RA collects information to
 								understand how the data is used and for justification of continued
-								funding</b> - Please read our <a target="_BLANK" href="info/privacy">privacy information</a></label>
+								funding</b> - Please read our <a target="_BLANK" href="info/privacy">privacy
+								information</a></label>
 
 					</div>
 					<br />
